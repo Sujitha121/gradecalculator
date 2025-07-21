db6c1ac9680d5e1f1efcad50dc03f4557e5b85b8
Sujitha R <sujitha.r@s.amity.edu>
2025-07-21 21:31:26 +0530

    C3

--- test.c
+++ test.c
@@ -6,4 +6,10 @@ char calculateGrade(float average) {
     if (average >= 90) return 'A';
     else if (average >= 80) return 'B';
     else if (average >= 70) return 'C';
-    else if (average >= 60) return 'D';
+    else if (averaeg >= 60) return 'D';
+    else return 'F';
+}
+
+int main() {
+    string studentName;
+    int numSubjects;
