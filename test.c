39fc74c065b05656f02344c7c7aad5c9d20ab0eb
Sujitha R <sujitha.r@s.amity.edu>
2025-07-21 21:27:26 +0530

    C2

--- test.c
+++ test.c
@@ -2,4 +2,8 @@
 using namespace std;
 
 // Function to calculate grade
-
+char calculateGrade(float average) {
+    if (average >= 90) return 'A';
+    else if (average >= 80) return 'B';
+    else if (average >= 70) return 'C';
+    else if (average >= 60) return 'D';
